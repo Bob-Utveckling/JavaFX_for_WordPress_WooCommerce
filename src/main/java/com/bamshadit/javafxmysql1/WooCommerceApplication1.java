fec260622529859bd983dddbc5c01d718094b99a
Bob-Utveckling <development@bamshad.se>
2023-01-26 15:30:10 +0100

    Develop the load customers feature by having a loader for Customer List

--- src/main/java/com/bamshadit/javafxmysql1/WooCommerceApplication1.java
+++ src/main/java/com/bamshadit/javafxmysql1/WooCommerceApplication1.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 public class WooCommerceApplication1 extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(WooCommerceApplication1.class.getResource("load-order.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(WooCommerceApplication1.class.getResource("load-order.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(WooCommerceApplication1.class.getResource("load-all-customers.fxml"));
         //https://stackoverflow.com/questions/37200845/how-to-switch-scenes-in-javafx
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720, 540);
+        //Scene scene = new Scene(fxmlLoader.load(), 720, 540); //for load-order
+        Scene scene = new Scene(fxmlLoader.load(), 800, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/bamshadit/javafxmysql1/controller/CustomerController.java
+++ src/main/java/com/bamshadit/javafxmysql1/controller/CustomerController.java
@@ -1,4 +1,67 @@
 package com.bamshadit.javafxmysql1.controller;
 
+import com.bamshadit.javafxmysql1.model.Customer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerController {
+
+    @FXML
+    public void temp_getCustomers() {
+        System.out.println("Load customers");
+        initializeCols();
+        updateTable();
+    }
+
+    @FXML
+    TableView<Customer> CustomerTable;
+    @FXML
+    private TableColumn<Customer, String> FirstNameCol;
+    @FXML
+    private TableColumn<Customer, String> LastNameNameCol;
+    @FXML
+    private TableColumn<Customer, String> AddressCol;
+    @FXML
+    private TableColumn<Customer, String> CityCol;
+    @FXML
+    private TableColumn<Customer, String> PostCodeCol;
+    @FXML
+    private TableColumn<Customer, String> CountryCol;
+    @FXML
+    private TableColumn<Customer, String> EmailCol;
+    @FXML
+    private TableColumn<Customer, String> PhoneCol;
+    @FXML
+    private TableColumn<Customer, String> CurrencyCol;
+
+    public List<Customer> getCustomers() {
+        Customer c1 = new Customer("B","L","123 Södra","Nöd","44931","Swe","b@b.se","123","SEK");
+        Customer c2 = new Customer("B","L","123 Södra","Nöd","44931","Swe","b@b.se","123","SEK");
+        Customer c3 = new Customer("B","L","123 Södra","Nöd","44931","Swe","b@b.se","123","SEK");
+        Customer c4 = new Customer("B","L","123 Södra","Nöd","44931","Swe","b@b.se","123","SEK");
+        List<Customer> Customers = new ArrayList<>();
+        Customers.add(c1);
+        Customers.add(c2);
+        Customers.add(c3);
+        Customers.add(c4);
+        return Customers;
+    }
+
+    public void initializeCols() {
+        FirstNameCol.setCellValueFactory(new PropertyValueFactory<>("FirstName"));
+    }
+    public void updateTable() {
+        ObservableList<Customer> custList = FXCollections.observableArrayList();
+        custList.add(new Customer("B","L","123 Södra","Nöd","44931","Swe","b@b.se","123","SEK"));
+        custList.add(new Customer("B","L","123 Södra","Nöd","44931","Swe","b@b.se","123","SEK"));
+        CustomerTable.setItems(custList);
+    }
+
 }
